ca39b2d2031c17f5e4d0b656afcce1b65354c455
Imran Fazli <imr4nfazli@gmail.com>
2023-01-27 16:24:31 +0800

    no await

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: prefer_const_constructors, depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future main() async{
+Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  Firebase.initializeApp();
   
   runApp(const MyApp());
 }
